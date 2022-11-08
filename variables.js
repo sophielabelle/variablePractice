9d8d2d8b4314b5cb7e4a211c73a68a6de66011ec
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-08 16:55:50 -0700

    Save console logs

--- variables.js
+++ variables.js
@@ -10,4 +10,22 @@ var alive = true;
 var hasDog = true;
 var hasTenDogs = false;
 
-var birthdayYear
+var tallPlant = "begonia";
+var shortPlant = "petunia";
+var bushyPlant = "philodendron";
+
+var birthdayYear = 1927;
+var age = 35;
+var heightInInches = 62;
+
+var alive = false;
+var hasDog = false;
+var hasTenDogs = true;
+
+console.log(tallPlant);
+console.log(shortPlant);
+console.log(birthdayYear);
+console.log(heightInInches);
+console.log(alive);
+console.log(hasTenDogs);
+
