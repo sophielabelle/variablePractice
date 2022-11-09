7847a2963fca5c6c18af8d7dfbef667f0a61de38
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-11-09 08:48:46 -0700

    Make changes to Console.log format

--- variables.js
+++ variables.js
@@ -1,31 +1,38 @@
 var tallPlant = "pathos";
+console.log(tallPlant); // => "pathos"
+tall= "goldenPathos";
+console.log(tall); // => "goldenPathos"
+
 var shortPlant = "cactus";
+console.log(shortPlant); // => "cactus"
+short = "spickyCactus";
+console.log(short); // => "spickyCactus"
+
 var bushyPlant = "christmasCactus";
 
 var birthdayYear = 1997;
+console.log(birthdayYear); // => 1997
+year = 1927;
+console.log(year); // => 1927
+
+
 var age = 25;
+
 var heightInInches = 62;
+console.log(heightInInches); // => 62
+height = 65;
+console.log(height); // => 65
 
 var alive = true;
+console.log(alive); // => true
+living = false;
+console.log(living); // => false
+
 var hasDog = true;
+
 var hasTenDogs = false;
+console.log(hasTenDogs); // => false
+tenDogs = true;
+console.log(tenDogs); // => true
 
-var tallPlant = "begonia";
-var shortPlant = "petunia";
-var bushyPlant = "philodendron";
-
-var birthdayYear = 1927;
-var age = 35;
-var heightInInches = 62;
-
-var alive = false;
-var hasDog = false;
-var hasTenDogs = true;
-
-console.log(tallPlant);
-console.log(shortPlant);
-console.log(birthdayYear);
-console.log(heightInInches);
-console.log(alive);
-console.log(hasTenDogs);
 
